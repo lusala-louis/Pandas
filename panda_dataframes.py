a0a3fabe9006e4f613f9fbc07b68042c392380b8
lusala-louis <lusalalouis@gmail.com>
2021-12-18 11:31:09 +0300

    Working with dataframes in pandas

--- panda_dataframes.py
+++ panda_dataframes.py
@@ -1,6 +1,24 @@
 #Unlike series data frames are two dimensional
 import pandas as pd
+import openpyxl
 data = {'Gender' : ['F', 'M', 'M'], 'Emp_ID' : [ 'E01', 'E02', 'E03'], 'Age' : [25, 27, 25]}
 #Specify the order of the column and column parameters
 dataframe = pd.DataFrame(data,columns = ['Emp_ID', 'Gender', 'Age'])
 print(dataframe)
+print(' ')
+#Loading data(csv format) into the pandas library from a file
+df = pd.read_csv('pokemon_data.csv')
+print(df) #Loads in all the data
+print(' ')
+print(df.head(5)) #Loads in only the top 5 rows of the data 
+print(' ')
+print(df.tail(5)) #Loads in only the bottom 5 rows of the data
+print(' ')
+#Loading data(xlsx format) into the pandas library from a file
+df_xlsx = pd.read_excel('pokemon_data.xlsx')
+print(df_xlsx.head(4))
+print(' ')
+#Loading data(txt format) into the pandas library from a file
+df_txt = pd.read_csv('pokemon_data.txt', delimiter = '\t')
+print(df_txt.head(4))
+print(' ')
