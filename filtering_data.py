2ed07b47fbd125cb798f8872dc2ac86a0ef15f3e
lusala-louis <lusalalouis@gmail.com>
2021-12-21 12:33:31 +0300

    Included some regular expressions in the data

--- filtering_data.py
+++ filtering_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import openpyxl
+import re
 df = pd.read_csv('pokemon_data.csv')
 
 #Filter out specific columns
@@ -15,8 +16,15 @@ print(' ')
 
 #Accesssing specific strings in the data
 with_mega = df.loc[df['Name'].str.contains('Mega')]
-print(with_mega)
+print(with_mega.head(7))
 print(' ')
 without_mega = df.loc[~df['Name'].str.contains('Mega')]
-print(without_mega.head(20))
+print(without_mega.head(6))
 print(' ')
+
+#Using regular expressions
+df.loc[df['Type 1'].str.contains('Fire | Grass', regex = True)]  #Gets the type 1 columns with fire or grass only
+print(df)
+#print(fire_grass)
+#print(' ')
+print(df.loc[df['Name'].str.contains('^pi[a-z]* ', flags=re.I, regex = True)])  #Gets the Name column containing names starting with pi 
